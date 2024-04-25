0c2fbdbc655cb983c7e3adc3346bfbd82dce5709
Vaidehi Pate <120419659+vsmp47@users.noreply.github.com>
2024-04-25 21:04:20 +0530

    Update 3_perceptron_evenodd.py

--- 3_perceptron_evenodd.py
+++ 3_perceptron_evenodd.py
@@ -1,3 +1,8 @@
+'''Write a Python Program using Perceptron Neural Network to recognise even and odd numbers.
+Given numbers are in ASCII form 0 to 9
+'''
+
+
 import numpy as np
 
 # Define the input-output mapping
